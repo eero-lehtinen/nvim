9276eabda30406220ab050f944d42608395a513c
Eero Lehtinen <eero@eerolehtinen.fi>
2023-08-28 05:53:39 +0300

    Add dressing.nvim to make vim ui select better

--- init.lua
+++ init.lua
@@ -280,6 +280,11 @@ require('lazy').setup({
     }
   },
 
+  {
+    'stevearc/dressing.nvim',
+    opts = {},
+  },
+
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
