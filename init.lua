0e958205e33b21a9a5fa713a71d81497d9473493
Eero Lehtinen <eero@eerolehtinen.fi>
2023-08-28 14:59:56 +0300

    Add repeat to leap

--- init.lua
+++ init.lua
@@ -259,6 +259,9 @@ require('lazy').setup({
 
   {
     'ggandor/leap.nvim',
+    dependencies = {
+      'tpope/vim-repeat'
+    },
     config = function()
       require('leap').add_default_mappings()
     end
