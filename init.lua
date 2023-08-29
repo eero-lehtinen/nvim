ad8b06911970d33abeccfbbbed0213ed15815289
Eero Lehtinen <eero@eerolehtinen.fi>
2023-08-30 00:58:24 +0300

    Set default tab behaviour and listchars

--- init.lua
+++ init.lua
@@ -343,8 +343,6 @@ vim.keymap.set('n', '<C-j>', '<C-^>', { desc = 'Alternate toggle' })
 -- See `:help vim.o`
 -- NOTE: You can change these options as you wish!
 
-vim.o.scrolloff = 8
-
 -- Set highlight on search
 vim.o.hlsearch = false
 
@@ -385,6 +383,11 @@ vim.o.termguicolors = true
 -- vim.g.loaded_netrw = 1
 -- vim.g.loaded_netrwPlugin = 1
 vim.g.nvim_tree_disable_netrw = 0
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = false
+vim.o.scrolloff = 8
+vim.o.listchars = "tab:» ,extends:›,precedes:‹,space:·,trail:·,eol:↲"
 
 -- [[ Basic Keymaps ]]
 
