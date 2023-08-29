cba85dd62b9d788f096119b6ff7b9e7bac690842
Eero Lehtinen <eero@eerolehtinen.fi>
2023-08-29 20:42:44 +0300

    Remove all vim regex highlighting to maybe fix some rust coloring issues
    Sometimes I got issues where test modules would have almost all code be
    red.

--- init.lua
+++ init.lua
@@ -449,7 +449,10 @@ require('nvim-treesitter.configs').setup {
   ignore_install = {},
   modules = {},
 
-  highlight = { enable = true },
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
   indent = { enable = true },
   incremental_selection = {
     enable = true,
