3fa0ca1b448710f329e5173e828e7190301f7b3b
Eero Lehtinen <eero@eerolehtinen.fi>
2023-08-30 19:00:54 +0300

    Simplify surround keybinds (actually wasn't that much overlap with leap)

--- init.lua
+++ init.lua
@@ -266,16 +266,8 @@ require('lazy').setup({
     },
     opts = {
       keymaps = {
-        insert          = '<C-g>z',
-        insert_line     = 'gC-ggZ',
-        normal          = 'gz',
-        normal_cur      = 'gZ',
-        normal_line     = 'gzgz',
-        normal_cur_line = 'gZgZ',
-        visual          = 'gz',
-        visual_line     = 'gZ',
-        delete          = 'gzd',
-        change          = 'gzc',
+        visual      = 'gs', -- was S
+        visual_line = 'gS', -- was gS
       },
     },
   },
