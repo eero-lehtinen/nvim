8d7495139ce3c9682933c19aeb1a46459f36e871
Eero Lehtinen <eero@eerolehtinen.fi>
2023-08-31 17:34:21 +0300

    Remove diffview stuff

--- init.lua
+++ init.lua
@@ -307,8 +307,6 @@ require('lazy').setup({
       },
     },
   },
-  -- maybe later
-  -- 'sindrets/diffview.nvim',
 
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
