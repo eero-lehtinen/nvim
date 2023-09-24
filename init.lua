8634592190ffd63658086278b4206dfa502fbfbc
Eero Lehtinen <eero@eerolehtinen.fi>
2023-09-25 00:29:00 +0300

    Formatting

--- init.lua
+++ init.lua
@@ -177,6 +177,7 @@ require('lazy').setup({
     -- branch = '0.1.x',
     dependencies = {
       'nvim-lua/plenary.nvim',
+      'debugloop/telescope-undo.nvim',
       -- Fuzzy Finder Algorithm which requires local dependencies to be built.
       -- Only load if `make` is available. Make sure you have the system
       -- requirements installed.
@@ -533,10 +534,19 @@ require('telescope').setup {
       find_command = { 'rg', '--files', '--hidden', '-g', '!.git' },
     },
   },
+  extensions = {
+    undo = {
+      layout_strategy = 'vertical',
+      layout_config = {
+        preview_height = 0.7,
+      },
+    },
+  },
 }
 
 -- Enable telescope fzf native, if installed
 pcall(require('telescope').load_extension, 'fzf')
+require('telescope').load_extension 'undo'
 
 -- See `:help telescope.builtin`
 vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
@@ -550,6 +560,7 @@ vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { de
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 vim.keymap.set('n', '<leader>sk', require('telescope.builtin').keymaps, { desc = '[S]earch [K]eymaps' })
+vim.keymap.set('n', '<leader>u', require('telescope').extensions.undo, { desc = '[U]ndo tree' })
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
