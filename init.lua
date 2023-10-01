9bbe9df8f0f1d7ca339c1c36e3a55dfca68c73ab
Eero Lehtinen <eero@eerolehtinen.fi>
2023-10-02 01:35:40 +0300

    Add buffer cmp source as backup

--- init.lua
+++ init.lua
@@ -62,7 +62,7 @@ require('lazy').setup({
       -- Snippet Engine & its associated nvim-cmp source
       { 'L3MON4D3/LuaSnip', build = 'make install_jsregexp' },
       'saadparwaiz1/cmp_luasnip',
-      -- 'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-buffer',
       'hrsh7th/cmp-path',
       'hrsh7th/cmp-cmdline',
 
