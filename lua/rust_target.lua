f242ba1dff8b4c50908c8185277ab9e1eb4ca1d1
Eero Lehtinen <eero@eerolehtinen.fi>
2023-11-04 03:35:14 +0200

    Shorter rust targets

--- lua/rust_target.lua
+++ lua/rust_target.lua
@@ -1,9 +1,9 @@
 local rust_targets = {
   wasm = 'wasm32-unknown-unknown',
-  windows = 'x86_64-pc-windows-msvc',
-  macos = 'x86_64-apple-darwin',
+  win = 'x86_64-pc-windows-msvc',
+  mac = 'x86_64-apple-darwin',
   linux = 'x86_64-unknown-linux-gnu',
   auto = nil,
 }
 
-return rust_targets.windows
+return rust_targets.auto
