a6f85cfe129d7afd4ca3a49c0c1dfa5f7f63e3ab
Eero Lehtinen <eero@eerolehtinen.fi>
2023-12-01 17:58:10 +0200

    Set copilot completion color

--- lua/custom/plugins/themes.lua
+++ lua/custom/plugins/themes.lua
@@ -34,6 +34,7 @@ return {
           PmenuSel = { fg = 'NONE', bg = theme.ui.bg_p2 },
           PmenuSbar = { bg = theme.ui.bg_m1 },
           PmenuThumb = { bg = theme.ui.bg_p2 },
+          CopilotSuggestion = { fg = '#739296' },
         }
       end,
     },
