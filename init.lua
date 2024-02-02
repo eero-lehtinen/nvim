a395bb7931741a5159be68fd12fd8bebaf213781
Eero Lehtinen <eero@eerolehtinen.fi>
2024-02-02 13:19:41 +0200

    Try to fix injected highlight in html files

--- init.lua
+++ init.lua
@@ -627,7 +627,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim', 'html', 'javascript', 'css' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = true,
@@ -645,7 +645,7 @@ require('nvim-treesitter.configs').setup {
     enable = true,
   },
 
-  highlight = { enable = true, additional_vim_regex_highlighting = true },
+  highlight = { enable = true, additional_vim_regex_highlighting = false },
   indent = { enable = true },
   incremental_selection = {
     enable = true,
