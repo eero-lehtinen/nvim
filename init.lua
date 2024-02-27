bb16cbd553b6168cb63c9a44cd4b76173d0628bd
Eero Lehtinen <eero@eerolehtinen.fi>
2024-02-28 00:04:44 +0200

    Make lualine hide things on small windows

--- init.lua
+++ init.lua
@@ -123,28 +123,50 @@ require('lazy').setup {
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
     -- See `:help lualine.txt`
-    opts = {
-      options = {
-        icons_enabled = true,
-        theme = 'auto',
-        component_separators = '|',
-        section_separators = '',
-      },
-      sections = {
-        lualine_c = {
-          { 'filename', path = 1 },
+
+    config = function()
+      local function win_larger_than(n)
+        return function()
+          return vim.fn.winwidth(0) > n
+        end
+      end
+      require('lualine').setup {
+        options = {
+          icons_enabled = true,
+          theme = 'auto',
+          component_separators = '',
+          section_separators = '',
         },
-        lualine_x = { 'encoding', 'fileformat', 'filetype', 'filesize' },
-      },
-      extensions = {
-        'fugitive',
-        'lazy',
-        'mason',
-        'nvim-dap-ui',
-        'nvim-tree',
-        'quickfix',
-      },
-    },
+        sections = {
+          lualine_c = {
+            { 'filename', path = 1 },
+          },
+          lualine_x = {
+            {
+              'encoding',
+              cond = win_larger_than(90),
+            },
+            {
+              'fileformat',
+              cond = win_larger_than(90),
+            },
+            { 'filetype' },
+            {
+              'filesize',
+              cond = win_larger_than(110),
+            },
+          },
+        },
+        extensions = {
+          'fugitive',
+          'lazy',
+          'mason',
+          'nvim-dap-ui',
+          'nvim-tree',
+          'quickfix',
+        },
+      }
+    end,
   },
 
   {
