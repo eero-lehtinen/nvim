38126f72313623870483920911d2604c69a1b6c0
Eero Lehtinen <eero@eerolehtinen.fi>
2024-03-03 23:01:13 +0200

    Add justfile commentstring

--- init.lua
+++ init.lua
@@ -464,6 +464,7 @@ vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
   pattern = 'justfile',
   callback = function()
     vim.bo.syntax = 'sh'
+    vim.bo.commentstring = '# %s'
   end,
 })
 
