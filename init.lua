f693ecfd3b69d093f52b247c84ecf47e99c17b57
Eero Lehtinen <eero@eerolehtinen.fi>
2024-03-03 13:08:53 +0200

    Remove whitespace

--- init.lua
+++ init.lua
@@ -414,29 +414,18 @@ vim.o.showmode = false
 vim.o.splitright = true
 vim.o.splitbelow = true
 -- vim.o.inccommand = 'split'
-
 vim.o.hlsearch = false
-
 vim.o.number = true
-
 vim.o.mouse = 'a'
-
 vim.o.clipboard = 'unnamedplus'
-
 vim.o.breakindent = true
-
 vim.o.undofile = true
-
 vim.o.ignorecase = true
 vim.o.smartcase = true
-
 vim.wo.signcolumn = 'yes'
-
 vim.o.updatetime = 250
 vim.o.timeoutlen = 300
-
 vim.o.completeopt = 'menuone,noselect'
-
 vim.o.termguicolors = true
 
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
