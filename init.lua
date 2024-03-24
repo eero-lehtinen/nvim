86a94c3a99d98d722f4f6c5d4af88df2f5180e42
Eero Lehtinen <eero@eerolehtinen.fi>
2024-03-24 03:15:48 +0200

    Add ron comment string

--- init.lua
+++ init.lua
@@ -488,5 +488,13 @@ vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
   end,
 })
 
+-- Setup ron commentstring
+vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
+  pattern = '*.ron',
+  callback = function()
+    vim.bo.commentstring = '// %s'
+  end,
+})
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
