80582729c10bb77526cefb016f47bab7ae5a0a65
Eero Lehtinen <eero@eerolehtinen.fi>
2024-04-16 21:59:25 +0300

    Stop using custom telescope cause it errors pretty often

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -16,244 +16,245 @@ return {
   },
   config = function()
     local actions = require 'telescope.actions'
-
-    local Layout = require 'nui.layout'
-    local Popup = require 'nui.popup'
-
+    --
+    -- local Layout = require 'nui.layout'
+    -- local Popup = require 'nui.popup'
+    --
     local telescope = require 'telescope'
-    local TSLayout = require 'telescope.pickers.layout'
-
-    local function make_popup(options)
-      local popup = Popup(options)
-      ---@diagnostic disable-next-line: inject-field
-      function popup.border:change_title(title)
-        popup.border.set_text(popup.border, 'top', title)
-      end
-      ---@diagnostic disable-next-line: param-type-mismatch
-      return TSLayout.Window(popup)
-    end
-
-    local function create_layout(picker)
-      local border = {
-        prompt = {
-          top_left = '┌',
-          top = '─',
-          top_right = '┬',
-          right = '│',
-          bottom_right = '',
-          bottom = '',
-          bottom_left = '',
-          left = '│',
-        },
-        prompt_patch = {
-          minimal = {
-            top_left = '┌',
-            top_right = '┐',
-          },
-          horizontal = {
-            top_left = '┌',
-            top_right = '┬',
-          },
-          vertical = {
-            top_left = '├',
-            top_right = '┤',
-          },
-        },
-        results = {
-          top_left = '├',
-          top = '─',
-          top_right = '┤',
-          right = '│',
-          bottom_right = '┘',
-          bottom = '─',
-          bottom_left = '└',
-          left = '│',
-        },
-        results_patch = {
-          minimal = {
-            bottom_right = '┘',
-          },
-          horizontal = {
-            bottom_right = '┴',
-          },
-          vertical = {
-            bottom_right = '┘',
-          },
-        },
-        preview = {
-          top_left = '┌',
-          top = '─',
-          top_right = '┐',
-          right = '│',
-          bottom_right = '┘',
-          bottom = '─',
-          bottom_left = '└',
-          left = '│',
-        },
-        preview_patch = {
-          minimal = {},
-          horizontal = {
-            bottom = '─',
-            bottom_left = '',
-            bottom_right = '┘',
-            left = '',
-            top_left = '',
-          },
-          vertical = {
-            bottom = '',
-            bottom_left = '',
-            bottom_right = '',
-            left = '│',
-            top_left = '┌',
-          },
-        },
-      }
-
-      local results = make_popup {
-        focusable = false,
-        border = {
-          style = border.results,
-          text = {
-            top = picker.results_title,
-            top_align = 'center',
-          },
-        },
-        win_options = {
-          winhighlight = 'Normal:Normal,FloatBorder:TelescopeBorder,FloatTitle:TelescopeResultsTitle',
-        },
-      }
-
-      local prompt = make_popup {
-        enter = true,
-        border = {
-          style = border.prompt,
-          text = {
-            top = picker.prompt_title,
-            top_align = 'center',
-          },
-        },
-        win_options = {
-          winhighlight = 'Normal:Normal,FloatBorder:TelescopeBorder,FloatTitle:TelescopePromptTitle',
-        },
-      }
-
-      local preview = make_popup {
-        focusable = false,
-        border = {
-          style = border.preview,
-          text = {
-            top = picker.preview_title,
-            top_align = 'center',
-          },
-        },
-        win_options = {
-          winhighlight = 'Normal:Normal,FloatBorder:TelescopeBorder,FloatTitle:TelescopePreviewTitle',
-        },
-      }
-
-      local box_by_kind = {
-        vertical = Layout.Box({
-          Layout.Box(preview, { grow = 1 }),
-          Layout.Box(prompt, { size = 2 }),
-          Layout.Box(results, { grow = 1 }),
-        }, { dir = 'col' }),
-        horizontal = Layout.Box({
-          Layout.Box({
-            Layout.Box(prompt, { size = 2 }),
-            Layout.Box(results, { grow = 1 }),
-          }, { dir = 'col', size = '50%' }),
-          Layout.Box(preview, { size = '50%' }),
-        }, { dir = 'row' }),
-        minimal = Layout.Box({
-          Layout.Box(prompt, { size = 2 }),
-          Layout.Box(results, { grow = 1 }),
-        }, { dir = 'col' }),
-      }
-
-      local minimal_pickers = {
-        ['Key Maps'] = true,
-        ['Commands'] = true,
-      }
-
-      local function get_box()
-        local strategy = picker.layout_strategy
-        if strategy == 'vertical' or strategy == 'horizontal' then
-          return box_by_kind[strategy], strategy
-        end
-
-        local height, width = vim.o.lines, vim.o.columns
-        local box_kind = 'horizontal'
-        if width < 110 then
-          box_kind = 'vertical'
-          if height < 40 then
-            box_kind = 'minimal'
-          end
-        end
-
-        if minimal_pickers[picker.prompt_title] then
-          box_kind = 'minimal'
-        end
-
-        return box_by_kind[box_kind], box_kind
-      end
-
-      local function prepare_layout_parts(layout, box_type)
-        layout.results = results
-        ---@diagnostic disable-next-line: undefined-field
-        results.border:set_style(border.results_patch[box_type])
-
-        layout.prompt = prompt
-        ---@diagnostic disable-next-line: undefined-field
-        prompt.border:set_style(border.prompt_patch[box_type])
-
-        if box_type == 'minimal' then
-          layout.preview = nil
-        else
-          layout.preview = preview
-          ---@diagnostic disable-next-line: undefined-field
-          preview.border:set_style(border.preview_patch[box_type])
-        end
-      end
-
-      local function get_layout_size(box_kind)
-        if picker.prompt_title == 'Current Buffer Fuzzy' then
-          return {
-            width = '90%',
-            height = '60%',
-          }
-        end
-        return picker.layout_config[box_kind == 'minimal' and 'vertical' or box_kind].size
-      end
-
-      local box, box_kind = get_box()
-      local layout = Layout({
-        relative = 'editor',
-        position = '50%',
-        size = get_layout_size(box_kind),
-      }, box)
-
-      ---@diagnostic disable-next-line: inject-field
-      layout.picker = picker
-      prepare_layout_parts(layout, box_kind)
-
-      local layout_update = layout.update
-      ---@diagnostic disable-next-line: duplicate-set-field
-      function layout:update()
-        ---@diagnostic disable-next-line: redefined-local
-        local box, box_kind = get_box()
-        prepare_layout_parts(layout, box_kind)
-        layout_update(self, { size = get_layout_size(box_kind) }, box)
-      end
-
-      ---@diagnostic disable-next-line: param-type-mismatch
-      return TSLayout(layout)
-    end
+    -- local TSLayout = require 'telescope.pickers.layout'
+    --
+    -- local function make_popup(options)
+    --   local popup = Popup(options)
+    --   ---@diagnostic disable-next-line: inject-field
+    --   function popup.border:change_title(title)
+    --     popup.border.set_text(popup.border, 'top', title)
+    --   end
+    --   ---@diagnostic disable-next-line: param-type-mismatch
+    --   return TSLayout.Window(popup)
+    -- end
+    --
+    -- local function create_layout(picker)
+    --   local border = {
+    --     prompt = {
+    --       top_left = '┌',
+    --       top = '─',
+    --       top_right = '┬',
+    --       right = '│',
+    --       bottom_right = '',
+    --       bottom = '',
+    --       bottom_left = '',
+    --       left = '│',
+    --     },
+    --     prompt_patch = {
+    --       minimal = {
+    --         top_left = '┌',
+    --         top_right = '┐',
+    --       },
+    --       horizontal = {
+    --         top_left = '┌',
+    --         top_right = '┬',
+    --       },
+    --       vertical = {
+    --         top_left = '├',
+    --         top_right = '┤',
+    --       },
+    --     },
+    --     results = {
+    --       top_left = '├',
+    --       top = '─',
+    --       top_right = '┤',
+    --       right = '│',
+    --       bottom_right = '┘',
+    --       bottom = '─',
+    --       bottom_left = '└',
+    --       left = '│',
+    --     },
+    --     results_patch = {
+    --       minimal = {
+    --         bottom_right = '┘',
+    --       },
+    --       horizontal = {
+    --         bottom_right = '┴',
+    --       },
+    --       vertical = {
+    --         bottom_right = '┘',
+    --       },
+    --     },
+    --     preview = {
+    --       top_left = '┌',
+    --       top = '─',
+    --       top_right = '┐',
+    --       right = '│',
+    --       bottom_right = '┘',
+    --       bottom = '─',
+    --       bottom_left = '└',
+    --       left = '│',
+    --     },
+    --     preview_patch = {
+    --       minimal = {},
+    --       horizontal = {
+    --         bottom = '─',
+    --         bottom_left = '',
+    --         bottom_right = '┘',
+    --         left = '',
+    --         top_left = '',
+    --       },
+    --       vertical = {
+    --         bottom = '',
+    --         bottom_left = '',
+    --         bottom_right = '',
+    --         left = '│',
+    --         top_left = '┌',
+    --       },
+    --     },
+    --   }
+    --
+    --   local results = make_popup {
+    --     focusable = false,
+    --     border = {
+    --       style = border.results,
+    --       text = {
+    --         top = picker.results_title,
+    --         top_align = 'center',
+    --       },
+    --     },
+    --     win_options = {
+    --       winhighlight = 'Normal:Normal,FloatBorder:TelescopeBorder,FloatTitle:TelescopeResultsTitle',
+    --     },
+    --   }
+    --
+    --   local prompt = make_popup {
+    --     enter = true,
+    --     border = {
+    --       style = border.prompt,
+    --       text = {
+    --         top = picker.prompt_title,
+    --         top_align = 'center',
+    --       },
+    --     },
+    --     win_options = {
+    --       winhighlight = 'Normal:Normal,FloatBorder:TelescopeBorder,FloatTitle:TelescopePromptTitle',
+    --     },
+    --   }
+    --
+    --   local preview = make_popup {
+    --     focusable = false,
+    --     border = {
+    --       style = border.preview,
+    --       text = {
+    --         top = picker.preview_title,
+    --         top_align = 'center',
+    --       },
+    --     },
+    --     win_options = {
+    --       winhighlight = 'Normal:Normal,FloatBorder:TelescopeBorder,FloatTitle:TelescopePreviewTitle',
+    --     },
+    --   }
+    --
+    --   local box_by_kind = {
+    --     vertical = Layout.Box({
+    --       Layout.Box(preview, { grow = 1 }),
+    --       Layout.Box(prompt, { size = 2 }),
+    --       Layout.Box(results, { grow = 1 }),
+    --     }, { dir = 'col' }),
+    --     horizontal = Layout.Box({
+    --       Layout.Box({
+    --         Layout.Box(prompt, { size = 2 }),
+    --         Layout.Box(results, { grow = 1 }),
+    --       }, { dir = 'col', size = '50%' }),
+    --       Layout.Box(preview, { size = '50%' }),
+    --     }, { dir = 'row' }),
+    --     minimal = Layout.Box({
+    --       Layout.Box(prompt, { size = 2 }),
+    --       Layout.Box(results, { grow = 1 }),
+    --     }, { dir = 'col' }),
+    --   }
+    --
+    --   local minimal_pickers = {
+    --     ['Key Maps'] = true,
+    --     ['Commands'] = true,
+    --   }
+    --
+    --   local function get_box()
+    --     local strategy = picker.layout_strategy
+    --     if strategy == 'vertical' or strategy == 'horizontal' then
+    --       return box_by_kind[strategy], strategy
+    --     end
+    --
+    --     local height, width = vim.o.lines, vim.o.columns
+    --     local box_kind = 'horizontal'
+    --     if width < 110 then
+    --       box_kind = 'vertical'
+    --       if height < 40 then
+    --         box_kind = 'minimal'
+    --       end
+    --     end
+    --
+    --     if minimal_pickers[picker.prompt_title] then
+    --       box_kind = 'minimal'
+    --     end
+    --
+    --     return box_by_kind[box_kind], box_kind
+    --   end
+    --
+    --   local function prepare_layout_parts(layout, box_type)
+    --     layout.results = results
+    --     ---@diagnostic disable-next-line: undefined-field
+    --     results.border:set_style(border.results_patch[box_type])
+    --
+    --     layout.prompt = prompt
+    --     ---@diagnostic disable-next-line: undefined-field
+    --     prompt.border:set_style(border.prompt_patch[box_type])
+    --
+    --     if box_type == 'minimal' then
+    --       layout.preview = nil
+    --     else
+    --       layout.preview = preview
+    --       ---@diagnostic disable-next-line: undefined-field
+    --       preview.border:set_style(border.preview_patch[box_type])
+    --     end
+    --   end
+    --
+    --   local function get_layout_size(box_kind)
+    --     if picker.prompt_title == 'Current Buffer Fuzzy' then
+    --       return {
+    --         width = '90%',
+    --         height = '60%',
+    --       }
+    --     end
+    --     return picker.layout_config[box_kind == 'minimal' and 'vertical' or box_kind].size
+    --   end
+    --
+    --   local box, box_kind = get_box()
+    --   local layout = Layout({
+    --     relative = 'editor',
+    --     position = '50%',
+    --     size = get_layout_size(box_kind),
+    --   }, box)
+    --
+    --   ---@diagnostic disable-next-line: inject-field
+    --   layout.picker = picker
+    --   prepare_layout_parts(layout, box_kind)
+    --
+    --   local layout_update = layout.update
+    --   ---@diagnostic disable-next-line: duplicate-set-field
+    --   function layout:update()
+    --     ---@diagnostic disable-next-line: redefined-local
+    --     local box, box_kind = get_box()
+    --     prepare_layout_parts(layout, box_kind)
+    --     layout_update(self, { size = get_layout_size(box_kind) }, box)
+    --   end
+    --
+    --   ---@diagnostic disable-next-line: param-type-mismatch
+    --   return TSLayout(layout)
+    -- end
 
     telescope.setup {
       defaults = {
         path_display = { 'truncate' },
-        layout_strategy = 'flex',
+        -- layout_strategy = 'flex',
         layout_config = {
+          prompt_position = 'top',
           horizontal = {
             size = {
               width = '90%',
@@ -267,7 +268,7 @@ return {
             },
           },
         },
-        create_layout = create_layout,
+        -- create_layout = create_layout,
         sorting_strategy = 'ascending',
         mappings = {
           i = {
