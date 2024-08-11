b8f38eb5630db3070e8a1a5758b791c64076dd72
Eero Lehtinen <eero@eerolehtinen.fi>
2024-08-11 15:04:40 +0300

    Set virtualedit to block

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -30,6 +30,7 @@ vim.o.updatetime = 250
 vim.o.timeoutlen = 300
 vim.o.completeopt = 'menuone,noselect'
 vim.o.termguicolors = true
+vim.o.virtualedit = 'block'
 
 vim.diagnostic.config {
   virtual_text = {
