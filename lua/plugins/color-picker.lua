e3e5df7b0ea0b95e2d4233c6aa555b0f5fe708ac
Eero Lehtinen <eero@eerolehtinen.fi>
2024-09-15 15:43:35 +0300

    Update color picker

--- lua/plugins/color-picker.lua
+++ lua/plugins/color-picker.lua
@@ -1,15 +1,18 @@
 return {
   {
-    -- 'eero-lehtinen/oklch-color-picker.nvim',
-    dir = '~/repos/oklch-color-picker.nvim',
+    'eero-lehtinen/oklch-color-picker.nvim',
+    dev = false,
     build = 'download.lua',
-    config = function()
-      require('oklch-color-picker').setup {
-        -- log_level = vim.log.levels.DEBUG,
-      }
-
-      -- Color::new(0.8748, 0.1816, 0.9064)
-      vim.keymap.set('n', '<leader>p', require('oklch-color-picker').pick_under_cursor)
-    end,
+    keys = {
+      {
+        '<leader>p',
+        function()
+          require('oklch-color-picker').pick_under_cursor()
+        end,
+        desc = '[P]ick color',
+      },
+    },
+    cmd = 'ColorPickOklch',
+    opts = {},
   },
 }
