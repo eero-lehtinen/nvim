ea5e011965a1cb35618050bc94c3c83feeec996f
Eero Lehtinen <eero@eerolehtinen.fi>
2024-10-05 03:33:02 +0300

    Change snippet jump keys

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -217,25 +217,14 @@ return {
             cmp.abort()
             fallback()
           end,
-          ['<Tab>'] = cmp.mapping(function(fallback)
-            -- if cmp.visible() then
-            --   cmp.select_next_item()
-            -- else
-
+          ['<C-l>'] = cmp.mapping(function()
             if luasnip.locally_jumpable() then
               luasnip.jump()
-            else
-              fallback()
             end
           end, { 'i', 's' }),
-          ['<S-Tab>'] = cmp.mapping(function(fallback)
-            -- if cmp.visible() then
-            --   cmp.select_prev_item()
-            -- else
+          ['<C-h>'] = cmp.mapping(function()
             if luasnip.locally_jumpable(-1) then
               luasnip.jump(-1)
-            else
-              fallback()
             end
           end, { 'i', 's' }),
         },
