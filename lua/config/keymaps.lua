a641adf9105ef02357f0c69a2b4c1715f7b36db4
Eero Lehtinen <eero@eerolehtinen.fi>
2024-10-21 23:20:25 +0300

    Apply gj and gk in visual mode too

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -27,8 +27,8 @@ vim.keymap.set('i', '.', '.<c-g>u')
 vim.keymap.set('i', ';', ';<c-g>u')
 
 -- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set({ 'n', 'v' }, 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set({ 'n', 'v' }, 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 vim.keymap.set('n', '[d', function()
   vim.diagnostic.jump { count = -1 }
