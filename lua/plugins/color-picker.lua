cc4c26206af0e8133004d1af1281fb46d8c53ba0
Eero Lehtinen <eero@eerolehtinen.fi>
2024-11-05 00:42:33 +0200

    Add color picker changes

--- lua/plugins/color-picker.lua
+++ lua/plugins/color-picker.lua
@@ -3,8 +3,28 @@ return {
     'eero-lehtinen/oklch-color-picker.nvim',
     -- dev = true,
     config = function()
-      require('oklch-color-picker').setup {}
+      require('oklch-color-picker').setup {
+        -- log_level = vim.log.levels.DEBUG,
+      }
+      -- require('oklch-color-picker.highlight').perf_logging = true
       vim.keymap.set('n', '<leader>v', '<cmd>ColorPickOklch<cr>')
     end,
   },
+  -- {
+  --   'uga-rosa/ccc.nvim',
+  --   dev = true,
+  --   opts = {
+  --     highlighter = {
+  --       auto_enable = true,
+  --     },
+  --   },
+  -- },
+  -- {
+  --   'brenoprata10/nvim-highlight-colors',
+  --   dev = true,
+  --   opts = {
+  --     enable_var_usage = false,
+  --     enable_named_colors = false,
+  --   },
+  -- },
 }
