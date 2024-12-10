bcf799f2d1690904b4733cabf9d43458d0a16762
Eero Lehtinen <eero@eerolehtinen.fi>
2024-12-10 22:01:47 +0200

    Update blink cmp config

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -338,8 +338,9 @@ return {
         ['<Tab>'] = {},
         ['<S-Tab>'] = {},
       },
-      windows = {
-        autocomplete = {
+
+      completion = {
+        menu = {
           draw = {
             columns = { { 'label', 'label_description', gap = 1 }, { 'kind_icon', 'kind' } },
           },
@@ -347,16 +348,16 @@ return {
         documentation = {
           auto_show = true,
           auto_show_delay_ms = 300,
-          scrollbar = false,
+          update_delay_ms = 5,
         },
-        ghost_text = {
-          enabled = false,
-        },
+        -- ghost_text = {
+        --   enabled = false,
+        -- },
       },
-      highlight = {
+      appearance = {
         use_nvim_cmp_as_default = true,
+        nerd_font_variant = 'normal',
       },
-      nerd_font_variant = 'normal',
     },
   },
 }
