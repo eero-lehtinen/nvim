1ccbb659cd5c30b6d4151c80ed319cb14c5390ca
Eero Lehtinen <eero@eerolehtinen.fi>
2024-12-27 19:34:01 +0200

    Add smear cursor thing thing

--- lua/plugins/base.lua
+++ lua/plugins/base.lua
@@ -561,4 +561,16 @@ return {
   "tpope/vim-endwise",
 
   { "ActivityWatch/aw-watcher-vim", enabled = false },
+
+  {
+    "sphamba/smear-cursor.nvim",
+    enabled = false,
+    opts = {
+      stiffness = 0.8, -- 0.6      [0, 1]
+      trailing_stiffness = 0.5, -- 0.3      [0, 1]
+      distance_stop_animating = 0.5, -- 0.1      > 0
+      hide_target_hack = false, -- true     boolean
+      legacy_computing_symbols_support = true,
+    },
+  },
 }
