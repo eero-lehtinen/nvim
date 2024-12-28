d6c0cb46a6b31ddd9637e7e14a633679217072df
Eero Lehtinen <eero@eerolehtinen.fi>
2024-12-28 20:07:45 +0200

    Fix neovide text contrast

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -70,9 +70,11 @@ if vim.g.neovide then
   vim.g.neovide_scroll_animation_length = 0
   vim.g.neovide_scroll_animation_far_lines = 0
   vim.g.neovide_hide_mouse_when_typing = true
-  vim.g.neovide_cursor_animation_length = 0.01
+  vim.g.neovide_cursor_animation_length = 0.008
   vim.g.neovide_cursor_trail_size = 0.7
   vim.g.neovide_cursor_animate_in_insert_mode = false
+  vim.g.neovide_text_gamma = 0.8
+  vim.g.neovide_text_contrast = 0.1
 
   vim.g.neovide_scale_factor = 1.0
   local change_scale_factor = function(delta)
