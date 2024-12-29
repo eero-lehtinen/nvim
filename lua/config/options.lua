9546df179578cd22e4b24b33f3d678e90d706ecd
Eero Lehtinen <eero@eerolehtinen.fi>
2024-12-30 01:56:47 +0200

    Increase neovide animation

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -70,7 +70,7 @@ if vim.g.neovide then
   vim.g.neovide_scroll_animation_length = 0
   vim.g.neovide_scroll_animation_far_lines = 0
   vim.g.neovide_hide_mouse_when_typing = true
-  vim.g.neovide_cursor_animation_length = 0.008
+  vim.g.neovide_cursor_animation_length = 0.015
   vim.g.neovide_cursor_trail_size = 0.7
   vim.g.neovide_cursor_animate_in_insert_mode = false
   vim.g.neovide_text_gamma = 0.8
