4b5d2b1262a45eb9521ee83901cb2fa09009e7a7
Eero Lehtinen <eero@eerolehtinen.fi>
2025-01-05 13:10:12 +0200

    Use cr and bs for treesitter selection

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -58,9 +58,9 @@ return {
         incremental_selection = {
           enable = true,
           keymaps = {
-            init_selection = "<C-n>",
-            node_incremental = "<C-n>",
-            node_decremental = "<C-m>",
+            init_selection = "<CR>",
+            node_incremental = "<CR>",
+            node_decremental = "<BS>",
             -- scope_incremental = '<A-v>',
           },
         },
