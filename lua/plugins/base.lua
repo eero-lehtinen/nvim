059c30576574a50a8d90780d5aab1c3b8c2121ee
Eero Lehtinen <eero@eerolehtinen.fi>
2025-01-16 18:00:46 +0200

    Improve coerce keybinds

--- lua/plugins/base.lua
+++ lua/plugins/base.lua
@@ -416,12 +416,17 @@ return {
 
   {
     "gregorias/coerce.nvim",
-    keys = { { "cr" } },
+    event = "VeryLazy",
+    -- keys = { { "<leader>C" } },
     opts = {
+      default_mode_keymap_prefixes = {
+        normal_mode = "<leader>C",
+        visual_mode = "<leader>C",
+      },
       default_mode_mask = {
         normal_mode = true,
         motion_mode = false,
-        visual_mode = false,
+        visual_mode = true,
       },
     },
   },
