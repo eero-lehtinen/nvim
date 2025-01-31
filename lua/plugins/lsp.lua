a78ec8b4edb225dfcd4b18823ce44a5b7845be8d
Eero Lehtinen <eero@eerolehtinen.fi>
2025-01-31 15:25:25 +0200

    Add some ts options from lazyvim

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -169,7 +169,33 @@ return {
         pyright = {},
 
         -- ts_ls = {},
-        vtsls = {},
+        vtsls = {
+          -- complete_function_calls = true,
+          vtsls = {
+            enableMoveToFileCodeAction = true,
+            autoUseWorkspaceTsdk = true,
+            experimental = {
+              maxInlayHintLength = 30,
+              completion = {
+                enableServerSideFuzzyMatch = true,
+              },
+            },
+          },
+          typescript = {
+            updateImportsOnFileMove = { enabled = "always" },
+            suggest = {
+              -- completeFunctionCalls = true,
+            },
+            inlayHints = {
+              enumMemberValues = { enabled = true },
+              functionLikeReturnTypes = { enabled = true },
+              parameterNames = { enabled = "literals" },
+              parameterTypes = { enabled = true },
+              propertyDeclarationTypes = { enabled = true },
+              variableTypes = { enabled = false },
+            },
+          },
+        },
         -- eslint = {},
         svelte = {},
         tailwindcss = {},
