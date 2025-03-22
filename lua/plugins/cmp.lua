f08f6f268711dc8e1504f01c8319df494685b4ea
Eero Lehtinen <eero@eerolehtinen.fi>
2025-03-22 14:54:06 +0200

    Hide virtual text when blink is open

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -83,6 +83,20 @@ return {
           vim.notify("Copilot disabled")
         end
       end, { desc = "[T]oggle [A]I Copilot" })
+
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "BlinkCmpMenuOpen",
+        callback = function()
+          vim.b.copilot_suggestion_hidden = true
+        end,
+      })
+
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "BlinkCmpMenuClose",
+        callback = function()
+          vim.b.copilot_suggestion_hidden = false
+        end,
+      })
     end,
   },
   {
