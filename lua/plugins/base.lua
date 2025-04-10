d53649373871ee8489176d20ee2dfc35f9a100d7
Eero Lehtinen <eero.lehtinen@monad.fi>
2025-04-10 20:04:28 +0300

    Add yazi nvim back

--- lua/plugins/base.lua
+++ lua/plugins/base.lua
@@ -297,4 +297,24 @@ return {
       },
     },
   },
+
+  {
+    "mikavilpas/yazi.nvim",
+    event = "VeryLazy",
+    keys = {
+      {
+        "<leader>y",
+        "<cmd>Yazi<cr>",
+        desc = "Open yazi at the current file",
+      },
+    },
+    ---@type YaziConfig
+    opts = {
+      -- if you want to open yazi instead of netrw, see below for more info
+      open_for_directories = false,
+      keymaps = {
+        show_help = "<f1>",
+      },
+    },
+  },
 }
