9a2a1c4dd45fcc64a3bc2d2c753210a628afbd1b
Eero Lehtinen <eero.lehtinen@monad.fi>
2025-07-28 13:55:11 +0300

    Lazy load treesj

--- lua/plugins/base.lua
+++ lua/plugins/base.lua
@@ -161,41 +161,45 @@ return {
   },
   {
     "Wansmer/treesj",
-    event = "VeryLazy",
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
       {
         "echasnovski/mini.splitjoin",
         version = false,
         opts = { mappings = { toggle = "<leader>J" } },
+        keys = { { "<leader>J", desc = "Toggle [J]oin Mini" } },
       },
     },
-    init = function()
-      local function get_pos_lang()
-        local c = vim.api.nvim_win_get_cursor(0)
-        local range = { c[1] - 1, c[2], c[1] - 1, c[2] }
-        local buf = vim.api.nvim_get_current_buf()
-        local ok, parser = pcall(vim.treesitter.get_parser, buf, vim.treesitter.language.get_lang(vim.bo[buf].ft))
-        if not ok or not parser then
-          return ""
-        end
-        local current_tree = parser:language_for_range(range)
-        return current_tree:lang()
-      end
+    keys = {
+      {
+        "<leader>j",
+        function()
+          local function get_pos_lang()
+            local c = vim.api.nvim_win_get_cursor(0)
+            local range = { c[1] - 1, c[2], c[1] - 1, c[2] }
+            local buf = vim.api.nvim_get_current_buf()
+            local ok, parser = pcall(vim.treesitter.get_parser, buf, vim.treesitter.language.get_lang(vim.bo[buf].ft))
+            if not ok or not parser then
+              return ""
+            end
+            local current_tree = parser:language_for_range(range)
+            return current_tree:lang()
+          end
 
-      vim.keymap.set("n", "<leader>j", function()
-        local tsj_langs = require("treesj.langs")["presets"]
-        local lang = get_pos_lang()
-        if lang ~= "" and tsj_langs[lang] then
-          require("treesj").toggle()
-        else
-          require("mini.splitjoin").toggle()
-        end
-      end, { desc = "Toggle [J]oin Node" })
-    end,
-    config = function()
-      require("treesj").setup({ use_default_keymaps = false })
-    end,
+          local tsj_langs = require("treesj.langs")["presets"]
+          local lang = get_pos_lang()
+          if lang ~= "" and tsj_langs[lang] then
+            require("treesj").toggle()
+          else
+            require("mini.splitjoin").toggle()
+          end
+        end,
+        desc = "Toggle [J]oin Node",
+      },
+    },
+    opts = {
+      use_default_keymaps = false,
+    },
   },
   {
     "laytan/cloak.nvim",
