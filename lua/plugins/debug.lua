cacc1f9521abaf7e0eeaa5dd3513e752df93f5e1
Eero Lehtinen <eero.lehtinen@monad.fi>
2025-09-25 15:24:31 +0300

    Make dap more lazy

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -28,7 +28,79 @@ return {
     -- Add your own debuggers here
     "leoluz/nvim-dap-go",
   },
-  event = "VeryLazy",
+  keys = {
+    {
+      "<F5>",
+      function()
+        require("dap").continue()
+      end,
+      desc = "Debug: Start/Continue",
+    },
+    {
+      "<F1>",
+      function()
+        require("dap").step_into()
+      end,
+      desc = "Debug: Step Into",
+    },
+    {
+      "<F2>",
+      function()
+        require("dap").step_over()
+      end,
+      desc = "Debug: Step Over",
+    },
+    {
+      "<F3>",
+      function()
+        require("dap").step_out()
+      end,
+      desc = "Debug: Step Out",
+    },
+    {
+      "<leader>b",
+      function()
+        require("dap").toggle_breakpoint()
+      end,
+      desc = "Debug: Toggle Breakpoint",
+    },
+    {
+      "<leader>B",
+      function()
+        require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
+      end,
+      desc = "Debug: Set Breakpoint with condition",
+    },
+    {
+      "<leader>dq",
+      function()
+        require("dap").terminate()
+      end,
+      desc = "Debug: Quit",
+    },
+    {
+      "<leader>dl",
+      function()
+        require("dap").run_last()
+      end,
+      desc = "Debug: Run Last",
+    },
+    {
+      "<leader>dt",
+      function()
+        require("dapui").toggle()
+      end,
+      desc = "Debug: Toggle",
+    },
+    {
+      "<leader>de",
+      function()
+        require("dapui").eval()
+      end,
+      mode = { "n", "v" },
+      desc = "Debug: Eval (View data of the selected variable)",
+    },
+  },
   config = function()
     local dap = require("dap")
     local dapui = require("dapui")
@@ -42,30 +114,9 @@ return {
       handlers = {},
     })
 
-    -- Basic debugging keymaps, feel free to change to your liking!
-    vim.keymap.set("n", "<F5>", dap.continue, { desc = "Debug: Start/Continue" })
-    vim.keymap.set("n", "<F1>", dap.step_into, { desc = "Debug: Step Into" })
-    vim.keymap.set("n", "<F2>", dap.step_over, { desc = "Debug: Step Over" })
-    vim.keymap.set("n", "<F3>", dap.step_out, { desc = "Debug: Step Out" })
-    vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
-    vim.keymap.set("n", "<leader>B", function()
-      dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
-    end, { desc = "Debug: Set Breakpoint with condition" })
-    vim.keymap.set("n", "<leader>dq", dap.terminate, { desc = "Debug: Quit" })
-    vim.keymap.set("n", "<leader>dl", dap.run_last, { desc = "Debug: Run Last" })
-
     -- For more information, see |:help nvim-dap-ui|
     ---@diagnostic disable: missing-fields
     dapui.setup({})
-
-    -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
-    vim.keymap.set("n", "<leader>dt", dapui.toggle, { desc = "Debug: Toggle" })
-    vim.keymap.set(
-      { "n", "v" },
-      "<leader>de",
-      dapui.eval,
-      { desc = "Debug: Eval (View data of the selected variable)" }
-    )
 
     dap.listeners.after.event_initialized["dapui_config"] = dapui.open
     dap.listeners.before.event_terminated["dapui_config"] = dapui.close
