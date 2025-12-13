d63338961761e43c100d09584df6becd8aa9511f
Eero Lehtinen <eero@eerolehtinen.fi>
2025-12-13 17:02:51 +0200

    Use yazi for directories

--- lua/plugins/base.lua
+++ lua/plugins/base.lua
@@ -292,11 +292,14 @@ return {
     ---@type YaziConfig
     opts = {
       -- if you want to open yazi instead of netrw, see below for more info
-      open_for_directories = false,
+      open_for_directories = true,
       keymaps = {
         show_help = "<f1>",
       },
     },
+    init = function()
+      vim.g.loaded_netrwPlugin = 1
+    end,
   },
 
   {
