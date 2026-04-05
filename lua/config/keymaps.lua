a14405fcc345e27fa9c84fa68195d0aeac95d1bf
Eero Lehtinen <eero@eerolehtinen.fi>
2026-04-05 19:09:29 +0300

    Add difftool

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -321,3 +321,5 @@ vim.cmd("packadd nvim.undotree")
 vim.keymap.set("n", "<leader>u", function()
   require("undotree").open({ command = "70vnew" })
 end, { desc = "Open [U]ndotree" })
+
+vim.cmd("packadd nvim.difftool")
