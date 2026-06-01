65071afb976d4cd205958d303f7c43a727be7cfd
Eero Lehtinen <eero@eerolehtinen.fi>
2026-06-01 15:20:50 +0300

    Maybe fix nvim env on windows

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -62,6 +62,7 @@ if vim.g.is_windows then
   vim.o.shellcmdflag =
     "-NoProfile -NoLogo -NonInteractive -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';$PSStyle.OutputRendering='plaintext';Remove-Alias -Force -ErrorAction SilentlyContinue tee;"
 
+  vim.env.NVIM = vim.v.servername
   vim.o.shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
 
   vim.o.shellpipe = '2>&1 | %%{ "$_" } | tee %s; exit $LastExitCode'
